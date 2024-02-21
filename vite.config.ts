b8ba37ca84d3b38a9764f3e6a44a6af199eb1060
2lz4m3 <157938150+2lz4m3@users.noreply.github.com>
2024-02-21 16:34:16 +0000

    Add GitHub Pages base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /* eslint @typescript-eslint/no-unsafe-member-access: 'off' */
+  base: process.env.GITHUB_PAGES ? '/img2zip/' : '/',
   plugins: [react()],
 })
